9517c71f4d05b393e64f36178c7a01816ce8a242
feixiang1209 <107115118+feixiang1209@users.noreply.github.com>
2023-09-17 14:16:18 +0300

    Update 16s-qiime-qutopipeline.py

--- 16s-qiime-qutopipeline.py
+++ 16s-qiime-qutopipeline.py
@@ -1,10 +1,10 @@
 ############################
 # AUTHOR: Xiang Zhao
-# Department: Bioscience Core Lab
+# Department: Bioscience Core Lab, King Abdullah University of Science and technology
 # Email:  xiang.zhao@kaust.edu.sa
 # This script uses qiime module to analysis amplicon sequencing result.
-# This Pipeline, which works on either Ibex or workstation, can be used by beginners with zero bioinformatics background.
-# By inputting one command it will finish all the analysis automatically, generating the feature table and taxa-bar-plots. 
+# This Pipeline, can be used by beginners with zero bioinformatics background.
+# By inputting one command it will finish all the analysis automatically, generating the feature table, diversity analysis and taxa-bar-plots, Phylogenetic tree, etc.
 ###############################
 
 
